ceb75255b25954d8ba643a4cc5e5af68e26bf70e
vlad-prog93 <vladka_@bk.ru>
2025-01-25 23:27:31 +0300

    настроен работа b с .nest

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,13 +1,16 @@
 import { Module } from "@nestjs/common";
 import { HymnsModule } from './hymns/hymns.module';
 import { MongooseModule } from '@nestjs/mongoose'
-
+import { ConfigModule } from "@nestjs/config";
 
 @Module({
   controllers: [],
   providers: [],
-  imports: [MongooseModule.forRoot(`mongodb://${process.env.URL_MONGO || 'localhost'}/hymns`),
-    HymnsModule],
+  imports: [
+    MongooseModule.forRoot(process.env.MONGO || 'mongodb://127.0.0.1:27017/hymns'),
+    HymnsModule,
+    ConfigModule.forRoot()
+  ],
 
 })
 export class AppModule { }
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,20 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from 'src/app.module';
-import dotenv from 'dotenv'
 
 async function server() {
-  dotenv.config()
-  const PORT = process.env.PORT || 5000
+
   const app = await NestFactory.create(AppModule)
+  const port = process.env.PORT || 5001
 
   app.enableCors({ origin: process.env.CORS || 'localhost' })
-  // app.enableCors({
-  //   origin: true, // Allowed origins
-  //   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // Allowed methods
-  //   credentials: true, // Allow credentials (e.g., cookies)
-  //   allowedHeaders: 'Content-Type, Accept', // Allowed headers
-  // });
 
-  await app.listen(PORT, () => console.log(`Server has been started in PORT = ${PORT}`))
+  await app.listen(port, () => console.log(`Server has been started in PORT = ${port}`))
 }
 server()
